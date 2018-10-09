474840a97bc572359e99b0c6c1c025c2bc9f6f90
Emerald-Jayde Henao <emeraldjayde@gmail.com>
2018-10-09 12:14:15 -0400

    outline of cli

--- src/httpC-cli.c
+++ src/httpC-cli.c
@@ -2,19 +2,63 @@
 #include <stdio.h>
 #include "http.h"
 
-int main(/* int argc, char *argv[] */) {
-    char* hello = (char*)malloc(strlen("HELLO" + 1));
+#include <getopt.h>
 
-    strcpy(hello, "HELLO");
-    hello = concat(hello, " WORLD!");
-    hello = concat(hello, " WORLD!");
+void print_usage() {
+    printf("Usage: httpc get [-v] [-h key:value] URL | httpc post [-v] [-h key:value] [-d inline-data] [-f file] URL\n");
+    exit(2);
+}
 
-    printf("\n\n%s\n\n", hello);
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        print_usage();
+    }
 
-    free(hello);
-    
-    hello = NULL;
-    printf("\n\n%s\n\n", hello);
+    if (strncmp(argv[1], "get", 3) == 0) {
+
+    }
+
+    int option;
+
+    while ((option = getopt(argc, argv, "vh:df")) != -1) {
+        switch (option) {
+            case 'v':
+                printf("you want verbose");
+                break;
+            case 'h':
+                printf("you want to pass headers");
+                break;
+            case 'd':
+                printf("you want inline data");
+                break;
+            case 'f':
+                printf("you want file");
+                break;
+            default:
+                printf("error");
+        }
+    }
+
+    // Parse the url to get the individual parts
+    parse_url();
+
+    // open connection to server
+    make_connection();
+
+    // build a request object and send it
+    make_request();
+
+    // get a response and make a response object
+    fetch_response();
+
+    // close the connection
+    close(socket);
+
+    // if -v
+    make_verbose();
+
+    // return the response
+    return_response();
 
     return 0;
 }
